2984acb77fa3dee9c2d59a4fe583e8fa3db08548
Mtouat <153912701+Mtouat@users.noreply.github.com>
2023-12-18 00:59:30 +0100

    Afficher/Retirer couleurs
    Afficher le bloc du choix des couleurs de la lumière quand on clique sur " Oui "
    
    Retirer le bloc du choix des couleurs de la lumière quand on clique sur " Non "

--- app/src/main/java/fr/univparis8/etud/hydratereminder/Notifs_lumineuses.java
+++ app/src/main/java/fr/univparis8/etud/hydratereminder/Notifs_lumineuses.java
@@ -2,14 +2,19 @@ package fr.univparis8.etud.hydratereminder;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import fr.univparis8.etud.hydratereminder.databinding.FragmentNotifsLumineusesBinding;
+
 public class Notifs_lumineuses extends Fragment {
 
+    FragmentNotifsLumineusesBinding binding;
     public static Notifs_lumineuses newInstance() {
         Notifs_lumineuses fragment = new Notifs_lumineuses();
         return fragment;
@@ -23,6 +28,30 @@ public class Notifs_lumineuses extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_notifs_lumineuses, container, false);
+        binding = FragmentNotifsLumineusesBinding.inflate(inflater, container, false);
+        binding.groupeCouleurs.setVisibility(View.INVISIBLE);
+        binding.couleurChoisie.setVisibility(View.INVISIBLE);
+        return binding.getRoot();
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+
+        binding.btnOuiNotifLum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.groupeCouleurs.setVisibility(View.VISIBLE);
+                binding.couleurChoisie.setVisibility(View.VISIBLE);
+            }
+        });
+
+        binding.btnNonNotifLum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.groupeCouleurs.setVisibility(View.INVISIBLE);
+                binding.couleurChoisie.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+
 }
