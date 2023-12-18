64a653fdfac2b00241e00d324d9a6785bb615ce5
Fiofionana <148996616+Fiofionana@users.noreply.github.com>
2023-12-18 12:16:31 +0100

    nouvelle modif du 18/12 12h16

--- app/src/main/java/fr/univparis8/etud/hydratereminder/Recalibrage1.java
+++ app/src/main/java/fr/univparis8/etud/hydratereminder/Recalibrage1.java
@@ -2,29 +2,51 @@ package fr.univparis8.etud.hydratereminder;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import fr.univparis8.etud.hydratereminder.databinding.FragmentChangementDeGourdeBinding;
+import fr.univparis8.etud.hydratereminder.databinding.FragmentRecalibrage1Binding;
+
 
 public class Recalibrage1 extends Fragment {
 
-
-    public static Recalibrage1 newInstance() {
-        Recalibrage1 fragment = new Recalibrage1();
-        return fragment;
-    }
+    private FragmentRecalibrage1Binding binding;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_recalibrage1, container, false);
+        binding = FragmentRecalibrage1Binding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+
+        binding.iBPageSuivante.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getParentFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                Recalibrage2 recalibrage2 = new Recalibrage2();
+                fragmentTransaction.replace(R.id.fragment_container_view, recalibrage2);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
     }
 }
