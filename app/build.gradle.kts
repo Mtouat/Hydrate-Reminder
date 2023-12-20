5867731152af453e08792bbc29cf442ee7e3c201
Fiofionana <148996616+Fiofionana@users.noreply.github.com>
2023-12-20 12:49:47 +0100

    modifs apres envoi apk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     defaultConfig {
         applicationId = "fr.univparis8.etud.hydratereminder"
         minSdk = 30
-        targetSdk = 30
+        targetSdk = 31
         versionCode = 1
         versionName = "1.0"
 
